48911935ac3c5317f10aedd7452e8e102ef18cc2
Shakthi Sachintha <43114582+shakthisachintha@users.noreply.github.com>
2022-08-14 23:07:23 +0530

    modifi

--- src/controllers/AuthController.php
+++ src/controllers/AuthController.php
@@ -25,9 +25,9 @@ class AuthController extends BaseController
     {
         
         $user = new User();
-        $user->set_email("shakthisachintha@gmail.com");
-        $user->set_name("Shakthi Sachintha");
-        $user->set_address("15/5A, Maitipe, Galle");
+        $user->set_email("shaktm");
+        $user->set_name("sa");
+        $user->set_address("itipe, Galle");
         $arr = $user->attributes_to_array();
         $this->databaseService->create_record('user', $arr);
         // print_r($arr);
